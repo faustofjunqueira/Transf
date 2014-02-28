3fe46573d4e299e9a2075bc8544d117edf6aece6
Fausto <faustoferjunqueira@gmail.com>
2014-02-28 14:03:59 -0300

    Reajustado os arquivos!

--- python/teste/udpC.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import socket
-from sys import *
-
-HOST, PORT = "localhost",9090
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(bytes(str(10),"utf-8"), (HOST, PORT))
-
